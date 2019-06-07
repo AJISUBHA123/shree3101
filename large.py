dd3582bf9976946af798d308f18681b6b9e37d0f
AJISUBHA123 <42169827+AJISUBHA123@users.noreply.github.com>
2019-06-07 09:46:33 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value"))
+b=int(input("enter the value"))
+c=int(input("enter the value"))
+if(a>=b)&(a>=c):
+                print(a)
+elif(b>=a)&(b>=c):
+                print(b)
+else:
+                print(c)
