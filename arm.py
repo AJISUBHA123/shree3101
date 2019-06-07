3146e88a02af07319812a3fd1da48a03fe536799
AJISUBHA123 <42169827+AJISUBHA123@users.noreply.github.com>
2019-06-07 13:37:56 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,11 +1,11 @@
-a=int(input())
-s=0
-t=a
-while t>0:
-        d=t%10
-        s+=d**3
-        t//=10
-if a==s:
+v=int(input())
+ss=0
+temp=v
+while temp>0:
+        d=temp%10
+        ss+=d**3
+        temp//=10
+if v==ss:
         print("yes")
 else:
         print("no")
